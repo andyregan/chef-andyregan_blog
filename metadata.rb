c58e4dae240a9581b8912fa062f6a79ad32102f5
Andrew Regan <andrewjregan@gmail.com>
2013-01-11 14:31:08 -0200

    fixing template attribute names

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "andrew@andyregan.net"
 license          "All rights reserved"
 description      "Installs/Configures andyregan_blog"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.7"
+version          "0.1.8"
 depends          "nginx"
 depends          "git"
