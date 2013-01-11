a7a8cb7000b92bb2876e4745519e96124e80ce92
Andrew Regan <andrewjregan@gmail.com>
2013-01-11 13:50:29 -0200

    fixing attribute typo

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
 default['andyregan_blog']['server_name'] = 'andyregan.net'
-default['andyregan_blog']['docroot'] = "/srv/www/#{default['andyregan_blog']['server_name']}/public"
+default['andyregan_blog']['docroot'] = "/srv/www/#{node['andyregan_blog']['server_name']}/public"
