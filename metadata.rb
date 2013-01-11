59c8f66b9d306598da862fdbeab7f3b91f3c7ceb
Vagrant <vagrant@vagrant-ubuntu.vagrantup.com>
2013-01-11 13:48:22 -0200

    adding dependency on git

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,4 @@ description      "Installs/Configures andyregan_blog"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 depends          "nginx"
+depends          "git"
