997239b4ede723999105e2d902ba8403939ab1cc
Andrew Regan <andrewjregan@gmail.com>
2013-01-12 08:40:37 -0200

    use application LWRP to deploy

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,6 @@
 default['andyregan_blog']['server_name'] = 'andyregan.net'
-default['andyregan_blog']['deploy_dir'] = "/srv/www/#{node['andyregan_blog']['server_name']}"
-default['andyregan_blog']['docroot'] = "#{node['andyregan_blog']['deploy_dir']}/public"
+default['andyregan_blog']['repository'] = "git://github.com/andyregan/andyregan.net.git"
+default['andyregan_blog']['revision'] = "HEAD"
+default['andyregan_blog']['application_port'] = 80
+default['andyregan_blog']['port'] = 80
+default['andyregan_blog']['docroot'] = "/srv/andyregan_blog/current/public"
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,43 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe "git"
+include_recipe "nginx"
 
-include_recipe "andyregan_blog::deploy_head"
-include_recipe "andyregan_blog::webserver"
+application_name = "andyregan_blog"
+app_node = node[application_name]
+root_path = "/srv/#{application_name}"
+
+application application_name do
+    path root_path
+    owner "nobody"
+    group "nogroup"
+    repository app_node[:repository]
+    revision app_node[:revision]
+
+    nginx_load_balancer do
+        application_port app_node[:application_port]
+        server_name app_node[:server_name]
+        port app_node[:port]
+        only_if { node['roles'].include?("#{application_name}_load_balancer") }
+    end
+end
+
+nginx_site "000-default" do
+  enable false
+end
+
+nginx_site "default" do
+    enable false
+end
+
+template "/etc/nginx/sites-available/#{node['andyregan_blog']['server_name']}" do
+  source "nginx_site.conf.erb"
+  only_if { node['roles'].include?("#{application_name}_application_server") }
+end
+
+nginx_site node['andyregan_blog']['server_name'] do
+  action :enable
+  only_if { node['roles'].include?("#{application_name}_application_server") }
+end
+
